85650299d774974295c456786bf5eb1615255414
tonyz <tonyz@UBUNTU.myguest.virtualbox.org>
2024-02-09 17:06:36 -0500

    Complete image filter

--- braitenberg/packages/solution/preprocessing.py
+++ braitenberg/packages/solution/preprocessing.py
@@ -1,8 +1,8 @@
 import cv2
 import numpy as np
 
-lower_hsv = np.array([171, 140, 100])
-upper_hsv = np.array([179, 200, 255])
+lower_hsv = np.array([000, 119, 75])
+upper_hsv = np.array([179, 255, 255])
 
 
 def preprocess(image_rgb: np.ndarray) -> np.ndarray:
